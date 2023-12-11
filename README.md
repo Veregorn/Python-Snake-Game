fb397cd4bf3890d1e91ec64355109aefecf50ebb
Veregorn <rjbarco@gmail.com>
2023-12-11 11:46:43 +0100

    3 whites squares snake created

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python-Snake-Game
 The classic mobile phones snake game developed in Python with Turtle graphics library
+
+The body of the snake starts with 3 squares
--- main.py
+++ main.py
@@ -6,6 +6,19 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+# Snake setup
+turtles = []
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+length = 3
+
+for index in range(0,length):
+    turtle = Turtle(shape="square")
+    turtle.color("white")
+    turtle.goto(starting_positions[index])
+    turtles.append(turtle)
+
+
+
 
 
 screen.exitonclick()
