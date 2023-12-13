5ce80a63ac6dab3de64edea79c1212595d8b2bd6
Veregorn <rjbarco@gmail.com>
2023-12-13 19:21:34 +0100

    Event Listeners created to move the snake

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ screen.tracer(0) # Turn turtle animation off so we can decide when to update the
 # Snake setup
 snake = snake.Snake()
 
+# Event Listeners
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 # Snake movement
 game_is_on = True
 
--- snake.py
+++ snake.py
@@ -1,24 +1,49 @@
 from turtle import Turtle
 
+# Let's create some constants so we can configure the game as we want lately
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
     
     def __init__(self) -> None:
         self.segments = []
-        self.starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-        self.length = len(self.starting_positions)
+        self.create_snake()
+        self.head = self.segments[0]
 
-        for index in range(0, self.length):
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
             segment = Turtle(shape="square")
             segment.color("white")
             segment.penup()
-            segment.goto(self.starting_positions[index])
+            segment.goto(position)
             self.segments.append(segment)
     
     def move(self):
         # This way our snake seems to have a solid one-piece body
-        for seg_num in range(self.length - 1, 0, -1):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
 
-        self.segments[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
+    
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
