52197d292028c831a2a65eabce3a69c8f8dd5c40
Veregorn <rjbarco@gmail.com>
2024-01-31 06:45:29 +0100

    Added a high score that read and write from a local file

--- scoreboard.py
+++ scoreboard.py
@@ -7,7 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = int(self.get_high_score())
         self.hideturtle()
         self.color("White")
         self.penup()
@@ -22,14 +22,23 @@ class Scoreboard(Turtle):
         self.score += 1
         self.clear()
         self.write_score()
-
-    """ def game_over(self):
-        self.goto(0, 0)
-        self.write('GAME OVER', align=ALIGNMENT, font=FONT) """
     
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.save_high_score()
 
         self.score = 0
         self.write_score()
+
+    def get_high_score(self):
+        high_score = ""
+        
+        with open("data.txt") as file:
+            high_score = file.read()
+
+        return high_score
+    
+    def save_high_score(self):
+        with open("data.txt", mode="w") as file:
+            file.write(str(self.high_score))
